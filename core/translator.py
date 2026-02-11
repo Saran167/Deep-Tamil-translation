c113fe65ba4c9d15f7fee034b8f5eb03adb8e4d3
Saran167 <148204034+Saran167@users.noreply.github.com>
2026-02-11 13:51:28 +0530

    Update translator.py

--- core/translator.py
+++ core/translator.py
@@ -2,10 +2,7 @@ from deep_translator import GoogleTranslator
 
 def translate_to_tamil(text):
     try:
-        translated = GoogleTranslator(
-            source="auto",
-            target="ta"
-        ).translate(text)
+        translated = GoogleTranslator(source="auto", target="ta").translate(text)
         return translated
-    except Exception:
+    except:
         return ""
