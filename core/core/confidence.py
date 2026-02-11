e44664e4ae856a0341e552445576e2906bed120d
Saran167 <148204034+Saran167@users.noreply.github.com>
2026-02-11 13:54:52 +0530

    Update confidence.py

--- core/core/confidence.py
+++ core/core/confidence.py
@@ -1,6 +1,4 @@
 def calculate_confidence(total_words, matched_words):
     if total_words == 0:
         return 0
-
-    score = (matched_words / total_words) * 100
-    return round(score, 2)
+    return round((matched_words / total_words) * 100, 2)
