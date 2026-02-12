29cd3a16977c39f8151f46fb3659162a3a40bdde
Saran167 <148204034+Saran167@users.noreply.github.com>
2026-02-12 08:56:44 +0530

    Update indictrans_translator.py

--- indictrans_translator.py
+++ indictrans_translator.py
@@ -1,20 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-class IndicTransTranslator:
-    def __init__(self):
-        # Load the IndicTrans model and tokenizer
-        self.model_name = "ai4bharat/indic-trans-encoder-decoder"
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
+model_name = "ai4bharat/indictrans2-en-indic-1B"
 
-    def translate(self, text, source_lang="ta", target_lang="ta"):
-        # Encode the input text
-        inputs = self.tokenizer.encode(text, return_tensors="pt")
-        # Perform translation
-        outputs = self.model.generate(inputs, max_length=512)
-        # Decode the output
-        translated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return translated_text
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True)
 
-# Create a single instance of the translator
-indictrans_translator = IndicTransTranslator()
+def translate_text(text):
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+    outputs = model.generate(**inputs, max_length=256)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
